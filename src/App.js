5a3d0cc745227bb8f666d61bbbf3f4e0cb394e22
Bifabig <biftubig@gmail.com>
2023-11-06 16:32:45 +0300

    add Layout component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
 import Members from './components/Members';
-import store from './redux/store';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Members />} />
-        </Routes>
-      </Router>
-    </Provider>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/members" element={<Members />} />
+      </Route>
+    </Routes>
   );
 }
 
