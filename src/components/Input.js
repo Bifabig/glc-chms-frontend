b1941fa8e4ec6fcac051c3a8583341c1024c0c2f
Bifabig <biftubig@gmail.com>
2023-11-07 11:55:56 +0300

    add input component

---
+++ src/components/Input.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from '../styles/Input.module.css';
+
+const Input = (props) => {
+  const {
+    name, value, placeholder, handleInput, type, step,
+  } = props;
+  return (
+    <input
+      className={styles.input}
+      name={name}
+      type={type}
+      value={value}
+      onChange={handleInput}
+      placeholder={placeholder}
+      step={step}
+      required
+    />
+  );
+};
+
+Input.defaultProps = {
+  step: '', // Add a default value for `step` prop
+};
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  handleInput: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
+  step: PropTypes.string,
+};
+
+export default Input;
