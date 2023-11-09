075ffac1ef49e5ad8364d35ab3535563f201f384
Bifabig <biftubig@gmail.com>
2023-11-09 16:59:12 +0300

    add new routes and update index.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Members from './components/Members/Members';
 import Layout from './components/Layout';
+import Churches from './components/Churches/Churches';
+import Teams from './components/Teams/Teams';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/members" element={<Members />} />
+        <Route path="/churches" element={<Churches />} />
+        <Route path="/teams" element={<Teams />} />
       </Route>
     </Routes>
   );
