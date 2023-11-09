5a62c02e5e34a9b621215580a52d057d25f96a5a
Bifabig <biftubig@gmail.com>
2023-11-09 16:59:31 +0300

    update thunk and store for new components

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import membersReducer from './reducers/membersSlice';
+import churchesReducer from './reducers/churchesSlice';
+import teamsReducer from './reducers/teamsSlice';
 
 const store = configureStore({
   reducer: {
     members: membersReducer,
+    churches: churchesReducer,
+    teams: teamsReducer,
   },
 });
 
--- src/redux/thunk.js
+++ src/redux/thunk.js
@@ -1,12 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const FETCH_MEMBERS_SUCCESS = 'FETCH_MEMBERS_SUCCESS';
-const url = '/api/v1/members';
+const url = '/api/v1';
 
-const getMembers = createAsyncThunk('members/getMembers', async (thunkAPI) => {
+// Members API
+export const getMembers = createAsyncThunk('members/getMembers', async (thunkAPI) => {
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(`${url}/members`);
     return response.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error);
@@ -17,7 +17,7 @@ export const createMember = createAsyncThunk(
   'members/createMember',
   async (memberData, { rejectWithValue }) => {
     try {
-      const response = await axios.post(url, memberData);
+      const response = await axios.post(`${url}/members`, memberData);
       return response.data;
     } catch (error) {
       return rejectWithValue('Unable to add memeber');
@@ -25,4 +25,47 @@ export const createMember = createAsyncThunk(
   },
 );
 
-export default getMembers;
+// Churches API
+export const getChurches = createAsyncThunk('churches/getChurches', async (thunkAPI) => {
+  try {
+    const response = await axios.get(`${url}/churches`);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
+  }
+});
+
+export const createChurch = createAsyncThunk(
+  'churches/createChurch',
+  async (churchData, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(`${url}/churches`, churchData);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue('Unable to add church');
+    }
+  },
+);
+
+// Teams API
+
+export const getTeams = createAsyncThunk('teams/getTeams', async (thunkAPI) => {
+  try {
+    const response = await axios.get(`${url}/teams`);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
+  }
+});
+
+export const createTeam = createAsyncThunk(
+  'teams/createTeam',
+  async (teamData, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(`${url}/teams`, teamData);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue('Unable to add team');
+    }
+  },
+);
