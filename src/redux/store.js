1e19774386bd7f0551683042ad56fe811fe163f6
Bifabig <biftubig@gmail.com>
2023-12-12 16:45:02 +0300

    add redux-persist package

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 import membersReducer from './reducers/membersSlice';
 import churchesReducer from './reducers/churchesSlice';
 import teamsReducer from './reducers/teamsSlice';
 import programsReducer from './reducers/programsSlice';
 import attendanceReducer from './reducers/attendancesSlice';
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  members: membersReducer,
+  churches: churchesReducer,
+  teams: teamsReducer,
+  programs: programsReducer,
+  attendances: attendanceReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const store = configureStore({
-  reducer: {
-    members: membersReducer,
-    churches: churchesReducer,
-    teams: teamsReducer,
-    programs: programsReducer,
-    attendances: attendanceReducer,
-  },
+  reducer: persistedReducer,
+  middleware: ((getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false,
+  })),
 });
 
 export default store;
