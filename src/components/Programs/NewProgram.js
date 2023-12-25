e4f8ebba175240366cfb084814cdc3d4940a8b60
Bifabig <biftubig@gmail.com>
2023-12-25 16:36:14 +0300

    modify part of the component using react hook forms

--- src/components/Programs/NewProgram.js
+++ src/components/Programs/NewProgram.js
@@ -23,34 +23,41 @@ const NewProgram = () => {
     setHasAttendance(e.target.checked);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const programData = {
-      name: name.current.value,
-      date: date.current.value,
-      church_id,
-      team_id,
-    };
-    const attendanceData = {
-      att_taker: attTaker.current.value,
-      program_id,
-    };
+  const onSubmit = (data) => {
+      const program = new FormData();
+      program.append('program[name]', data.name);
+      program.append('program[photo]', data.photo[0]);
+      program.append('program[address]', data.address);
+      program.append('program[phone_number]', data.phone_number);
+      program.append('program[joined_at]', data.joined_at);
+      program.append('program[church_id]', data.church_id);
+    // e.preventDefault();
+    // const programData = {
+    //   name: name.current.value,
+    //   date: date.current.value,
+    //   church_id,
+    //   team_id,
+    // };
+    // const attendanceData = {
+    //   att_taker: attTaker.current.value,
+    //   program_id,
+    // };
 
-    if (hasAttendance && attTaker.current.value !== '') {
-      dispatch(createProgram(programData));
-      dispatch(createAttendance(attendanceData))
-        .then(setMsg('Program Added Successfully'));
-      name.current.value = '';
-      date.current.value = '';
-      attTaker.current.value = '';
+    // if (hasAttendance && attTaker.current.value !== '') {
+    //   dispatch(createProgram(programData));
+    //   dispatch(createAttendance(attendanceData))
+    //     .then(setMsg('Program Added Successfully'));
+    //   name.current.value = '';
+    //   date.current.value = '';
+    //   attTaker.current.value = '';
 
-      navigate('/programs');
-    } else if (hasAttendance && attTaker.current.value === '') {
-      setMsg('Please add an attendance taker first');
-    } else {
-      dispatch(createProgram(programData)).then(setMsg('Program Added Successfully'));
-      name.current.value = '';
-      date.current.value = '';
+    //   navigate('/programs');
+    // } else if (hasAttendance && attTaker.current.value === '') {
+    //   setMsg('Please add an attendance taker first');
+    // } else {
+    //   dispatch(createProgram(programData)).then(setMsg('Program Added Successfully'));
+    //   name.current.value = '';
+    //   date.current.value = '';
 
       navigate('/programs');
     }
@@ -58,7 +65,7 @@ const NewProgram = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit} className="form">
+      <form onSubmit={handleSubmit(onSubmit)} className="form">
         <Input
           name="name"
           ref={name}
