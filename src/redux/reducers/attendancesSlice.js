3c1d472f9d7c7714b19204a1e337f9d001f829ed
Bifabig <biftubig@gmail.com>
2024-01-09 16:55:35 +0300

    remove unwanted comments from attendance slice

--- src/redux/reducers/attendancesSlice.js
+++ src/redux/reducers/attendancesSlice.js
@@ -28,18 +28,9 @@ const attendancesSlice = createSlice({
         state.errorMsg = action.payload;
       })
       .addCase(createAttendance.fulfilled, (state, action) => {
-        // console.log(action);
-        // state.attendances.filter((att) => console.log(att));
-        // const oldState = state.attendances;
-        // console.log(action.payload.attendance.data);
-        // const newAtt = action.payload.attendance;
-        // state.attendances.push(newAtt);
-        const data = [action.payload.attendance.data, ...state.attendances.data];
-        const included = [action.payload.attendance.included,
+        state.attendances.data = [action.payload.attendance.data, ...state.attendances.data];
+        state.attendances.included = [action.payload.attendance.included,
           ...state.attendances.included];
-        // state.attendances.data = Array.from(new Set(data));
-        state.attendances = { data, included };
-        // state.attendances.included = Array.from(new Set(included));
         state.isLoading = false;
         state.error = false;
       })
