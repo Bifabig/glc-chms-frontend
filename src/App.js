2568bf66fe087643bc35663fc2b4a742c4e91185
Bifabig <biftubig@gmail.com>
2024-01-16 17:05:42 +0300

    add protected routes

--- src/App.js
+++ src/App.js
@@ -10,23 +10,28 @@ import TeamDetail from './components/Teams/TeamDetail';
 import ProgramDetail from './components/Programs/ProgramDetail';
 import Signup from './components/Signup';
 import ConfirmationEmail from './components/EmailConfirmation';
+import Login from './components/Login';
+import ProtectedRoutes from './components/ProtectedRoutes';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/members" element={<Members />} />
-        <Route path="/members/:memberId" element={<MemberDetail />} />
-        <Route path="/churches" element={<Churches />} />
-        <Route path="/churches/:churchId" element={<ChurchDetail />} />
-        <Route path="/teams" element={<Teams />} />
-        <Route path="/teams/:teamId" element={<TeamDetail />} />
-        <Route path="/programs" element={<Programs />} />
-        <Route path="/programs/:programId" element={<ProgramDetail />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/confirmation/:confirmation_token" element={<ConfirmationEmail />} />
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/" element={<Layout />}>
+          <Route path="/members" element={<Members />} />
+          <Route path="/members/:memberId" element={<MemberDetail />} />
+          <Route path="/churches" element={<Churches />} />
+          <Route path="/churches/:churchId" element={<ChurchDetail />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/teams/:teamId" element={<TeamDetail />} />
+          <Route path="/programs" element={<Programs />} />
+          <Route path="/programs/:programId" element={<ProgramDetail />} />
+        </Route>
         {/* <Route path="/confirmation" element={<ConfirmationEmail />} /> */}
       </Route>
+      <Route path="/confirmation/:confirmation_token" element={<ConfirmationEmail />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
