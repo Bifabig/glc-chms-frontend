811076104e2b1006bc620459cd65558f806e0e0f
Bifabig <biftubig@gmail.com>
2024-01-17 17:33:06 +0300

    use react hook forms

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,98 +1,123 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { useDispatch, useSelector } from 'react-redux';
-import { useState } from 'react';
+// import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button } from '@mui/material';
+import {
+  Box, Button, Checkbox, FormControlLabel, TextField, Typography,
+} from '@mui/material';
+import { useForm } from 'react-hook-form';
 import { loginUserAsync, logoutUserAsync } from '../features/authentication/authenticationSlice';
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigates = useNavigate();
-  const { authToken } = useSelector((state) => state.auth);
+  const { isLoading, error } = useSelector((state) => state.auth);
+  const auth = localStorage.getItem('authToken');
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const [formData, setFormData] = useState({
-    // name: '',
-    email: '',
-    password: '',
-    // passwordConfirmation: '',
-    // type: '',
-  });
-
-  const handleLogout = () => {
-    dispatch(logoutUserAsync(formData));
-    localStorage.removeItem('authToken');
-    // navigates('/signup');
-  };
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmitForm = (event) => {
-    event.preventDefault();
-    dispatch(loginUserAsync(formData));
+  const onSubmit = (data) => {
+    const user = new FormData();
+    user.append('user[email]', data.email);
+    user.append('user[password]', data.password);
+    dispatch(loginUserAsync(user));
     navigates('/');
   };
 
-  return (
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(logoutUserAsync());
+  };
+
+  if (error) {
+    return (
+      <span>
+        Something Went Wrong...
+        <br />
+        <br />
+        {error.message}
+        <br />
+        Wrong Email / Password
+        <br />
+        If this is your first time logging in please make sure to confirm email first.
+      </span>
+    );
+  }
+
+  return isLoading ? <h2>Loading...</h2> : (
     <div>
-      {authToken !== null
+      {auth && auth.length > 0
         ? (
           <Button
             onClick={handleLogout}
             variant="contained"
-        // className={styles.updateBtn}
             size="small"
           >
             Logout
           </Button>
         )
         : (
-          <form
-            onSubmit={handleSubmitForm}
+          <Box
+            component="form"
+            onSubmit={handleSubmit(onSubmit)}
+            sx={{
+              maxWidth: '500px',
+              margin: 'auto',
+              padding: '20px',
+              borderRadius: '8px',
+              boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+              backgroundColor: 'white',
+            }}
           >
-            <h3>Login</h3>
-            <div>
-              {/* <input
-            placeholder="John Doe"
-            required
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-          /> */}
-              <input
-                placeholder="johndoe@example.com"
-                required
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleInputChange}
-              />
-              <input
-                required
-                type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleInputChange}
-                placeholder="Password"
-              />
-              {/* <input
-            required
-            type="password"
-            name="passwordConfirmation"
-            value={formData.passwordConfirmation}
-            onChange={handleInputChange}
-            placeholder="Confirm password"
-          /> */}
-            </div>
-            <button type="submit">
+
+            <Typography variant="h5" component="div" sx={{ mb: 2 }}>
+              Login Form
+            </Typography>
+            <TextField
+              fullWidth
+              label="email"
+              type="email"
+              {...register('email', {
+                required: 'email is required',
+              })}
+              error={Boolean(errors.email)}
+              helperText={errors.email?.message}
+              margin="normal"
+            />
+            <TextField
+              fullWidth
+              type="password"
+              label="Password"
+              {...register('password', {
+                required: 'Password is required',
+                minLength: {
+                  value: 6,
+                  message: 'Password must be at least 6 characters',
+                },
+              })}
+              error={Boolean(errors.password)}
+              helperText={errors.password?.message}
+              margin="normal"
+              sx={{ mt: 2 }}
+            />
+            <FormControlLabel
+              control={<Checkbox {...register('rememberMe')} color="primary" />}
+              label="Remember Me"
+              sx={{ mt: 1, textAlign: 'left' }}
+            />
+            <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
               Login
-            </button>
-          </form>
+            </Button>
+            <Box sx={{ mt: 2, textAlign: 'center' }}>
+              <Button onClick={() => navigates('/')} variant="body2">
+                Forgot Password?
+              </Button>
+              <Box mt={1}>
+                <Button onClick={() => navigates('/signup')} variant="body2">
+                  Don&apos;t have an account? Sign Up
+                </Button>
+              </Box>
+            </Box>
+          </Box>
         )}
     </div>
   );
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,78 +1,121 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { useDispatch } from 'react-redux';
-import { useState } from 'react';
+// import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import {
+  Box, Button, Checkbox, FormControlLabel, TextField, Typography,
+} from '@mui/material';
+import { useForm } from 'react-hook-form';
 import { registerUserAsync } from '../features/authentication/authenticationSlice';
 
 const RegistrationForm = () => {
   const dispatch = useDispatch();
   const navigates = useNavigate();
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    passwordConfirmation: '',
-    type: '',
-  });
+  // const [formData, setFormData] = useState({
+  //   name: '',
+  //   email: '',
+  //   password: '',
+  //   passwordConfirmation: '',
+  //   type: '',
+  // });
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+  // const handleInputChange = (event) => {
+  //   const { name, value } = event.target;
+  //   setFormData({
+  //     ...formData,
+  //     [name]: value,
+  //   });
+  // };
 
-  const handleSubmitForm = (event) => {
-    event.preventDefault();
-    dispatch(registerUserAsync(formData));
+  const onSubmit = (data) => {
+    // event.preventDefault();
+    const user = new FormData();
+    user.append('user[email]', data.email);
+    user.append('user[password]', data.password);
+    user.append('user[password_confirmation]', data.passwordConfirmation);
+    dispatch(registerUserAsync(user));
     navigates('/login');
   };
 
   return (
     <div>
-      <form
-        onSubmit={handleSubmitForm}
+      <Box
+        component="form"
+        onSubmit={handleSubmit(onSubmit)}
+        sx={{
+          maxWidth: '500px',
+          margin: 'auto',
+          padding: '20px',
+          borderRadius: '8px',
+          boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+          backgroundColor: 'white',
+        }}
       >
-        <h3>REGISTER</h3>
-        <div>
-          {/* <input
-            placeholder="John Doe"
-            required
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-          /> */}
-          <input
-            placeholder="johndoe@example.com"
-            required
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-          />
-          <input
-            required
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-            placeholder="Password"
-          />
-          <input
-            required
-            type="password"
-            name="passwordConfirmation"
-            value={formData.passwordConfirmation}
-            onChange={handleInputChange}
-            placeholder="Confirm password"
-          />
-        </div>
-        <button type="submit">
+
+        <Typography variant="h5" component="div" sx={{ mb: 2 }}>
+          REGISTER
+        </Typography>
+        <TextField
+          fullWidth
+          label="email"
+          type="email"
+          {...register('email', {
+            required: 'email is required',
+          })}
+          error={Boolean(errors.email)}
+          helperText={errors.email?.message}
+          margin="normal"
+        />
+        <TextField
+          fullWidth
+          type="password"
+          label="Password"
+          {...register('password', {
+            required: 'Password is required',
+            minLength: {
+              value: 6,
+              message: 'Password must be at least 6 characters',
+            },
+          })}
+          error={Boolean(errors.password)}
+          helperText={errors.password?.message}
+          margin="normal"
+          sx={{ mt: 2 }}
+        />
+        <TextField
+          fullWidth
+          type="password"
+          label="passwordConfirmation"
+          {...register('passwordConfirmation', {
+            required: 'passwordConfirmation is required',
+            minLength: {
+              value: 6,
+              message: 'passwordConfirmation must be at least 6 characters',
+            },
+          })}
+          error={Boolean(errors.passwordConfirmation)}
+          helperText={errors.passwordConfirmation?.message}
+          margin="normal"
+          sx={{ mt: 2 }}
+        />
+        <FormControlLabel
+          control={<Checkbox {...register('rememberMe')} color="primary" />}
+          label="Remember Me"
+          sx={{ mt: 1, textAlign: 'left' }}
+        />
+        <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
           Register
-        </button>
-      </form>
+        </Button>
+        <Box sx={{ mt: 2, textAlign: 'center' }}>
+          <Box mt={1}>
+            <Button onClick={() => navigates('/login')} variant="body2">
+              Have an account? Login
+            </Button>
+          </Box>
+        </Box>
+      </Box>
     </div>
   );
 };
