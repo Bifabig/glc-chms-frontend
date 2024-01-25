eb2eded1b5d39cf548f793be55f42e09d629969d
Bifabig <biftubig@gmail.com>
2024-01-25 17:20:24 +0300

    modify app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import Members from './components/Members';
 import Layout from './components/Layout';
 import Churches from './components/Churches';
@@ -15,30 +16,41 @@ import ProtectedRoutes from './components/ProtectedRoutes';
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
 import Calendar from './components/Calendar';
+import { ColorModeContext, useMode } from './theme';
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <Routes>
-      <Route element={<ProtectedRoutes />}>
-        <Route path="/" element={<Layout />}>
-          <Route path="/members" element={<Members />} />
-          <Route path="/members/:memberId" element={<MemberDetail />} />
-          <Route path="/churches" element={<Churches />} />
-          <Route path="/churches/:churchId" element={<ChurchDetail />} />
-          <Route path="/teams" element={<Teams />} />
-          <Route path="/teams/:teamId" element={<TeamDetail />} />
-          <Route path="/programs" element={<Programs />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/programs/:programId" element={<ProgramDetail />} />
-          <Route path="/signup" element={<Signup />} />
-        </Route>
-        {/* <Route path="/confirmation" element={<ConfirmationEmail />} /> */}
-      </Route>
-      <Route path="/confirmation/:confirmation_token" element={<ConfirmationEmail />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/forgotPassword" element={<ForgotPassword />} />
-      <Route path="/password/:reset_password_token" element={<ResetPassword />} />
-    </Routes>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/" element={<Layout />}>
+              <Route path="/members" element={<Members />} />
+              <Route path="/members/:memberId" element={<MemberDetail />} />
+              <Route path="/churches" element={<Churches />} />
+              <Route path="/churches/:churchId" element={<ChurchDetail />} />
+              <Route path="/teams" element={<Teams />} />
+              <Route path="/teams/:teamId" element={<TeamDetail />} />
+              <Route path="/programs" element={<Programs />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/programs/:programId" element={<ProgramDetail />} />
+              <Route path="/signup" element={<Signup />} />
+            </Route>
+            {/* <Route path="/confirmation" element={<ConfirmationEmail />} /> */}
+          </Route>
+          <Route path="/confirmation/:confirmation_token" element={<ConfirmationEmail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgotPassword" element={<ForgotPassword />} />
+          <Route path="/password/:reset_password_token" element={<ResetPassword />} />
+        </Routes>
+
+      </ThemeProvider>
+
+    </ColorModeContext.Provider>
+
   );
 }
 
