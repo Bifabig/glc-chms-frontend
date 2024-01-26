c4d67b7b0ca5445215a212ed22ddc16bd2915ded
Bifabig <biftubig@gmail.com>
2024-01-26 17:23:14 +0300

    add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { Box, Typography, useTheme } from '@mui/material';
+import PropTypes from 'prop-types';
+import { tokens } from '../theme';
+
+const Header = ({ title, subtitle }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box mb="30px">
+      <Typography
+        variant="h4"
+        color={colors.grey[100]}
+        fontWeight="bold"
+        sx={{ mb: '5px' }}
+      >
+        {title}
+      </Typography>
+      <Typography
+        variant="h6"
+        color={colors.greenAccent[400]}
+      >
+        {subtitle}
+      </Typography>
+    </Box>
+  );
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+};
+
+export default Header;
