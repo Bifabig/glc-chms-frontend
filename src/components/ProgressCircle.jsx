ec1bbd8c4a9f082b841714a50b0bc90908bfb531
Bifabig <biftubig@gmail.com>
2024-01-26 17:23:46 +0300

    add progress circke component

---
+++ src/components/ProgressCircle.jsx
@@ -0,0 +1,13 @@
+// import { useTheme } from '@emotion/react';
+// import { tokens } from '../theme';
+
+// const ProgressCircle = ({ progress = "0.75", size="40" }) => {
+//   const theme = useTheme();
+//   const colors = tokens(theme.palette.mode);
+
+//   return (
+//     <div>ProgressCircle</div>
+//   )
+// }
+
+// export default ProgressCircle;
