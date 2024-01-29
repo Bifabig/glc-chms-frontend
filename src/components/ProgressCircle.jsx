b73110ceee54b7164a754b228594cd5a2a6d3869
Bifabig <biftubig@gmail.com>
2024-01-29 16:24:34 +0300

    modify progress circle component

--- src/components/ProgressCircle.jsx
+++ src/components/ProgressCircle.jsx
@@ -1,13 +1,34 @@
-// import { useTheme } from '@emotion/react';
-// import { tokens } from '../theme';
+import { useTheme } from '@emotion/react';
+import { Box } from '@mui/material';
+import PropTypes from 'prop-types';
+import { tokens } from '../theme';
 
-// const ProgressCircle = ({ progress = "0.75", size="40" }) => {
-//   const theme = useTheme();
-//   const colors = tokens(theme.palette.mode);
+const ProgressCircle = ({ progress, size }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const angle = progress * 360;
 
-//   return (
-//     <div>ProgressCircle</div>
-//   )
-// }
+  return (
+    <Box sx={{
+      background: `radial-gradient(${colors.primary[400]} 55%, transparent 56%),
+        conic-gradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}deg 360deg),
+        ${colors.greenAccent[500]}`,
+      borderRadius: '50%',
+      width: `${size}px`,
+      height: `${size}px`,
+    }}
+    />
+  );
+};
 
-// export default ProgressCircle;
+ProgressCircle.defaultProps = {
+  size: '40',
+  progress: '0.75',
+};
+
+ProgressCircle.propTypes = {
+  progress: PropTypes.string,
+  size: PropTypes.string,
+};
+
+export default ProgressCircle;
